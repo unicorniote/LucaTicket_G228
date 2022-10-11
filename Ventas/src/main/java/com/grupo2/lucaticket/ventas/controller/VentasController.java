c79839c20f766f96cf2cd5c6441908d2050ca6b7
Lamia <99553152+Lhamia@users.noreply.github.com>
2022-10-11 13:49:36 +0200

    ventasservice y controller

--- Ventas/src/main/java/com/grupo2/lucaticket/ventas/controller/VentasController.java
+++ Ventas/src/main/java/com/grupo2/lucaticket/ventas/controller/VentasController.java
@@ -4,14 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.grupo2.lucaticket.ventas.service.VentasServiceI;
+
+
+/**
+* @Project LucaTicket
+*
+* @Classname VentasController
+*
+* @author Lamia
+* 
+* @date 11/10/2022
+*
+* @since 1.0
+*
+*/
+
 @RestController
 @RequestMapping("/ventas")
 public class VentasController {
-	@Autowired
-	private VentasRepository ventasRepository;
+
 	
 	@Autowired 
-	private VentasService ventasService;
+	private VentasServiceI ventasService;
 
 	
 	
